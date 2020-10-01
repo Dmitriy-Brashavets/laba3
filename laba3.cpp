c2d80ced4c99ff006d837b079194e4e48da634bf
Dmitriy <72145339+Dmitriy-Brashavets@users.noreply.github.com>
2020-10-01 03:11:05 +0300

    Update laba3.cpp

--- laba3.cpp
+++ laba3.cpp
@@ -18,10 +18,17 @@ int main()
 		data.push_back(word);
 	} while (!fin.eof());
 	fin.close();
+	for (auto i = 0; i < data.size(); i++)
+		cout << data[i] << endl;
+	cout << "\n";
 	for (int i = 0; i < data.size() - 1; i++)
 		for (int j = i + 1; j < data.size(); j++)
-			if (data[i] > data[j])
-				swap(data[i], data[j]);
+			if (strcmp(data[i].c_str(), data[j].c_str()) > 0) swap(data[i], data[j]);
+	for (int i = 0; i < data.size() - 1; i++)
+		for (int j = i + 1; j < data.size(); j++)
+			if (data[i].size() > data[j].size()) swap(data[i], data[j]);
+	for (auto i = 0; i < data.size(); i++)
+		cout << data[i] << endl;
 	ofstream fout;
 	fout.open(patch.c_str(), ios::trunc | ios::binary);
 	for (int i = 0; i < data.size(); i++)
