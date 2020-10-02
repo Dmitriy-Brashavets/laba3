fffea9aab7a0e79a2a2ef3f0980620986769f711
Dmitriy <72145339+Dmitriy-Brashavets@users.noreply.github.com>
2020-10-02 19:37:44 +0300

    Update laba3.cpp

--- laba3.cpp
+++ laba3.cpp
@@ -1,42 +1,59 @@
-#include <fstream>
-#include <iostream>
 #include <string>
-#include <vector>
+#include <vector> 
+#include <fstream> 
+#include <iostream> 
+
 using namespace std;
-
-void sort(vector<string>& v);
-
-int main()
-{
-    setlocale(0, "RUS");
-    ifstream fin;
-    string patch;
-    getline(cin, patch);
-    fin.open(patch.c_str(), ios::binary);
-    vector<string> data;
-    do {
-        string word;
-        do {
-            word.push_back(fin.get());
-        } while (word.back() != '\n' && !fin.eof());
-        word.pop_back();
-        data.push_back(word);
-    } while (!fin.eof());
-    fin.close();
-    sort(data);
-    ofstream fout;
-    fout.open(patch.c_str(), ios::trunc | ios::binary);
-    for (int i = 0; i < data.size(); i++)
-        fout << data[i].c_str() << '\n';
-    fout.close();
+vector<string> get_data(const string&);
+int main() {
+	string file = "data.txt";
+	int parameter;
+	cout << "Parameter: ";
+	cin >> parameter;
+	vector<string> data = get_data(file);
+	for (auto i = data.begin(); i < data.end(); i++)
+		if (i->size() < parameter) data.erase(i--);
+	vector<string> copy = data;
+	int count = 0;
+	for (int i = 0; i < data.size(); i++) {
+		count = 0;
+		for (int j = i + 1; j < data.size(); j++) {
+			bool flag_cmp = true;
+			for (int f = 0; f < parameter; f++) {
+				if (data[i][f] != data[j][f]) flag_cmp = false;
+				if (!flag_cmp) break;
+			}
+			if (flag_cmp) {
+				count++;
+				data.erase(begin(data) + j);
+				j--;
+			}
+		}
+		if (count) {
+			data.erase(begin(data) + i);
+			i--;
+		}
+	}
+	for (auto i = copy.begin(); i + 1 < copy.end(); i++)
+		for (auto j = i + 1; j < copy.end(); j++)
+			if (*i == *j)copy.erase(j--);
+	for (int i = 0; i < copy.size() - 1; i++)
+	{
+		for (int j = i + 1; j < copy.size(); j++)
+			if (copy[i] > copy[j]) swap(copy[i], copy[j]);
+		cout << copy[i] << endl;
+	}
+	return 0;
 }
 
-void sort(vector<string>& v)
-{
-    for (int i = 0; i < v.size() - 1; i++)
-        for (int j = i + 1; j < v.size(); j++)
-        {
-            if (v[i].size() > v[j].size()) swap(v[i], v[j]);
-            else if (v[i].size() == v[j].size() && v[i] > v[j]) swap(v[i], v[j]);
-        }
+vector<string> get_data(const string & s) {
+	ifstream file(s);
+	vector<string> data;
+	while (!file.eof()) {
+		string temp;
+		file >> temp;
+		data.push_back(temp);
+	}
+	file.close();
+	return data;
 }
